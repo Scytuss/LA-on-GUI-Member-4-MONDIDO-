46203cafed94e5da1bd66ddcf19a05f012f4222d
Scytuss <raulmondido13@gmail.com>
2026-04-04 04:19:22 +0800

    Add files via upload

---
+++ PasswordValidator/src/main/java/com/example/passwordvalidator/PasswordValidatorApplication.java
@@ -0,0 +1,18 @@
+package com.example.passwordvalidator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class PasswordValidatorApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(PasswordValidatorApplication.class.getResource("password-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Password Validation");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
