84ee4ba244038f02ec1680bc56ad3e2fe480feba
Scytuss <raulmondido13@gmail.com>
2026-04-04 04:33:42 +0800

    Add files via upload

---
+++ PasswordValidator/src/main/java/com/example/passwordvalidator/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.passwordvalidator;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(PasswordValidatorApplication.class, args);
+    }
+}
